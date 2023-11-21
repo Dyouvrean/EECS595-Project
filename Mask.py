1ba5331edb4e98d8b76430a56c0d5e489ecfc794
Dyouvrean <dry199984@outlook.com>
2023-11-20 20:32:11 -0500

    train

--- Mask.py
+++ Mask.py
@@ -1,47 +1,59 @@
 import pandas as pd
 import random
-import nltk
-from nltk.corpus import stopwords
-nltk.download('stopwords')
+from torch.utils.data import Dataset, DataLoader
+# from nltk.corpus import stopwords
+# nltk.download('stopwords')
+from transformers import AutoTokenizer
+
+class TextMaskingDataset(Dataset):
+    def __init__(self, file_path,tokenizer, mask_token="<mask>", mask_probability=0.15 ):
+        self.corpus = self.read_file(file_path)
+        # self.sw_nltk = stopwords.words('english')
+        self.mask_token = mask_token
+        self.mask_probability = mask_probability
+        self.tokenizer = tokenizer
+    def read_file(self, file):
+        with open(file, encoding='utf-8') as f:
+            corpus = [line.strip("\n") for line in f.readlines()]
+        return corpus
+
+    def mask_sentence(self, sentence):
+        print(sentence)
+        words = sentence.split()
+        masked_words = []
+        idx = random.randint(0, len(words)-1)
+        indice = []
+        origin=[]
+        for index,word in enumerate(words):
+            word = word.strip(",.!;:\n\t")
+            if index == idx:
+                masked_words.append(self.mask_token)
+                indice.append(True)
+            else:
+                masked_words.append(word)
+                indice.append(False)
+            origin.append(word)
+        return " ".join(masked_words), " ".join(origin), indice
+
+    def __len__(self):
+        return len(self.corpus)
+
+    def __getitem__(self, idx):
+        sentence = self.corpus[idx]
+        masked_sentence,origin, mask_indice = self.mask_sentence(sentence)
+        masked = self.tokenizer(masked_sentence, return_tensors="pt")
+        origin = self.tokenizer(origin, return_tensors="pt")
+        return masked, origin, mask_indice
 
 
 
 
-def read_file(file):
-    with open(file, encoding='utf-8') as f:
-         corpus = []
-         for line in f.readlines():
-             corpus.append(line.strip("\n"))
-    return corpus
-
-
-def sw_remove(row,sw):
-    res=[]
-    row = row.split(" ")
-    for word in row:
-        if word.lower() not in sw:
-            res.append(word)
-    return " ".join(res)
-
-
-
-
-def mask_sentence(sentence, sw,mask_token="<mask>", mask_probability=0.15):
-    words = sentence.split()
-    masked_words =[]
-    for word in words:
-        word= word.strip(",.\n\t")
-        if random.random()<mask_probability and word.lower() not in sw:
-            masked_words.append(mask_token)
-        else:
-            masked_words.append(word)
-    return " ".join(masked_words)
-
-
-
-corpus= read_file("ARC_Corpus.txt")[:200]
-corpus= pd.DataFrame(corpus, columns=['sentence'])
-sw_nltk = stopwords.words('english')
-#corpus["sw_removed"] = corpus.apply(lambda row: sw_remove(row['sentence'],sw_nltk),axis=1)
-corpus["masked"]=corpus.apply(lambda row: mask_sentence(row['sentence'],sw_nltk),axis=1)
-print(corpus.head())
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+# # Example usage
+dataset = TextMaskingDataset(file_path="ARC_Corpus.txt",tokenizer= tokenizer)[1]
+print(dataset)
+# dataloader = DataLoader(dataset, batch_size=4, shuffle=False,collate_fn=collate_fn)
+# for i, batch in enumerate(dataloader):
+#     print(batch)
+#     if i == 1:  # Stop after retrieving 2 batches
+#         break
